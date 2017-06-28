157903e340b013e81719e2f1c8e35d39bc3e09b3
qtotuan <quynh.totuan@gmail.com>
2017-06-28 15:48:11 -0400

    Done.

---
+++ spaceship.js
@@ -0,0 +1,21 @@
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.phasersCharge = "uncharged"
+    if (this.crew.length === 0) {
+      this.docked = true
+    } else {
+      this.docked = false
+    }
+    this.setCrew()
+  }
+
+  setCrew() {
+    this.crew.forEach( member => member.currentShip = this)
+  }
+}
